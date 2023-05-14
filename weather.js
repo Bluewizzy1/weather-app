0bf3c15ff4d94e93a7c656d94fb2e84c9b299faf
Onuchi Johnson <Onuchi.J@bullseye.local>
2023-05-14 13:35:42 +0100

    added new features

--- weather.js
+++ weather.js
@@ -1,14 +1,14 @@
 
 
-const apiKey = ""
-const apiUrl = ""
+const apiKey = "6d7485c2d5564e1fc3bba05e03c29cde";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
 const weatherIcon = document.querySelector(".weather-icon")
 
 async function checkweather(city){
-    const response = await fetch(apiUrl + city + `appid=${apiKey}`);
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
 
     if(response.status == 404){
         document.querySelector(".error").style.display = "block"
@@ -18,7 +18,7 @@ async function checkweather(city){
         var data = await response.json();
 
     document.querySelector(".city").innerHTML = data.name;
-    document.querySelector("temp").innerHTML = Math.round(data.main.temp) + "°C";
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
     document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
     document.querySelector(".wind").innerHTML = data.wind.speed + " km/hr";
 
@@ -35,15 +35,15 @@ async function checkweather(city){
         weatherIcon.src = "assets/drizzle.png";
     }
     else if(data.weather[0].main == "Mist"){
-        weatherIcon.src = "assets/mist.png";
+        weatherIcon.src = "assets/mist.png";  
     }
 
-    document.querySelector(".weather").style.display = "block"
+    document.querySelector(".weather").style.display = "block";
     document.querySelector(".error").style.display = "none"
     }
 
     
-};
+}
 
 searchBtn.addEventListener("click", ()=>{
     checkweather(searchBox.value);
